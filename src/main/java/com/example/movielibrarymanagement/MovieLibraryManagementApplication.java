47a645ec634e5b4c9c332b9d78bacc874e47df73
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-12 12:31:11 +0200

    chore: initial commit with dependencies and application.properties

---
+++ src/main/java/com/example/movielibrarymanagement/MovieLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.movielibrarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieLibraryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movielibrarymanagement/MovieLibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movielibrarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
