7a103937bfc6e0ea71e5edae51c85e804b8631c0
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-14 15:15:00 +0200

    feat: add register, login, auth response dto's

---
+++ src/main/java/com/example/movielibrarymanagement/dto/RegisterRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.movielibrarymanagement.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequestDto {
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 100)
+    private String password;
+
+}
