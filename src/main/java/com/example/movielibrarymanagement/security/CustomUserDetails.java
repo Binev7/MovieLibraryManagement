d624e4cc1fbdf2a40cd5d3b75c88be5820bc0e1e
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-14 12:21:32 +0200

    feat: add CustomUserDetails

---
+++ src/main/java/com/example/movielibrarymanagement/security/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package com.example.movielibrarymanagement.security;
+
+import com.example.movielibrarymanagement.model.User;
+import com.example.movielibrarymanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(Collections.singleton(new SimpleGrantedAuthority(user.getRole().name())))
+                .build();
+    }
+}
