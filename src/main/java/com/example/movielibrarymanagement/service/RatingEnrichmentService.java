772e02894ed49388ec0355b0e7010a67fe8258d0
Binev7 <georgi_binev.gogo@abv.bg>
2025-12-18 13:57:50 +0200

    refactor: fix RatingEnrichmentService bug for updating a movie

--- src/main/java/com/example/movielibrarymanagement/service/RatingEnrichmentService.java
+++ src/main/java/com/example/movielibrarymanagement/service/RatingEnrichmentService.java
@@ -27,13 +27,19 @@ public class RatingEnrichmentService {
 
         try {
             String ratingValue = omdbClient.fetchImdbRating(title);
-            Rating rating = ratingMapper.toEntity(ratingValue);
 
             movieRepository.findById(movieId).ifPresent(movie -> {
-                rating.setMovie(movie);
-                movie.setRating(rating);
-                movieRepository.save(movie);
+                Rating rating = movie.getRating();
 
+                if (rating == null) {
+                    rating = ratingMapper.toEntity(ratingValue);
+                    rating.setMovie(movie);
+                    movie.setRating(rating);
+                } else {
+                    rating.setValue(ratingValue != null ? ratingValue : "N/A");
+                }
+
+                movieRepository.save(movie);
                 log.info("Enriched movie ID: {} with rating: {}", movieId, rating.getValue());
             });
 
@@ -41,12 +47,17 @@ public class RatingEnrichmentService {
             log.error("Failed to enrich movie ID: {}. Reason: {}", movieId, e.getMessage());
 
             movieRepository.findById(movieId).ifPresent(movie -> {
-                Rating rating = ratingMapper.toEntity(null);
-                rating.setMovie(movie);
+                Rating rating = movie.getRating();
 
-                movie.setRating(rating);
+                if (rating == null) {
+                    rating = ratingMapper.toEntity(null);
+                    rating.setMovie(movie);
+                    movie.setRating(rating);
+                } else {
+                    rating.setValue("N/A");
+                }
+
                 movieRepository.save(movie);
-
                 log.info("Set N/A rating for movie ID: {} due to enrichment failure", movieId);
             });
         }
